ee89efdbf54a6897fe01cb5ffca6a1908955ee8a
Mohammad Aslam <aslampaasa420@gmail.com>
2026-09-25 02:44:13 +0530

    hello world program

---
+++ 1_hello_world/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
+
+/**
+ * Commands:
+ * 1. Build the program:
+ *    - go build 1_hello_world/main.go
+ *
+ * 2. Run the generated executable:
+ *    - ./main
+ *
+ * 3. Or directly run the Go file:
+ *    - go run 1_hello_world/main.go
+ */
