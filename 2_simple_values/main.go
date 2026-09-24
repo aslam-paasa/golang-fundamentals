ddc6914a2285d2a3b60ba8a83061df9d6459f419
Mohammad Aslam <aslampaasa420@gmail.com>
2026-09-25 02:49:02 +0530

    basic data types in go

---
+++ 2_simple_values/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/**
+	 * Basic Data Types in Go
+	 */
+
+	/**
+	 * 1. Integer (int)
+	 *    - Represents whole numbers.
+	 *    - Example: 1, 10, 100
+	 */
+	fmt.Println(1 + 1)
+
+	/**
+	 * 2. String (string)
+	 *    - Represents text.
+	 *    - Example: "hello golang"
+	 */
+	fmt.Println("hello golang")
+
+	/**
+	 * 3. Boolean (bool)
+	 *    - Represents either true or false.
+	 *    - Example: true, false
+	 */
+	fmt.Println(true)
+	fmt.Println(false)
+
+	/**
+	 * 4. Float (float)
+	 *    - Represents numbers with decimal values.
+	 *    - Example: 10.5, 7.25
+	 */
+	fmt.Println(10.5)
+	fmt.Println(7.0 / 3.0)
+}
